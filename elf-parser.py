c5263280cb4b00cc8979a30027ab2b95f0518210
kiya-z <2267161320@qq.com>
2016-03-01 18:22:41 +0800

    read file

--- elf-parser.py
+++ elf-parser.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from utils import read_file
+
 if __name__ == '__main__':
-    with open("/home/nagain/kiya/parse-elf/libmobisec.so", 'rb') as f:
-        print f.read(52)
+    # with open(r"/home/nagain/kiya/parse-elf/libmobisec.so",'rb') as f:
+    #     print f.read(52)
+    print read_file(r"/home/nagain/kiya/parse-elf/libmobisec.so",52)
