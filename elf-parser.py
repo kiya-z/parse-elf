699ba6dded62d80059d1cee2182d35eae82669c5
kiya-z <2267161320@qq.com>
2016-03-02 09:51:53 +0800

    update

--- elf-parser.py
+++ elf-parser.py
@@ -4,6 +4,4 @@
 from utils import read_file
 
 if __name__ == '__main__':
-    # with open(r"/home/nagain/kiya/parse-elf/libmobisec.so",'rb') as f:
-    #     print f.read(52)
-    print read_file(r"/home/nagain/kiya/parse-elf/libmobisec.so",52)
+    print read_file(r"/home/nagain/kiya/parse-elf/libmobisec.so",16)
