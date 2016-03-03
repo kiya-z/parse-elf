209c24fd5912d9c764c146089a5b5758bfa9cb4a
kiya-z <2267161320@qq.com>
2016-03-03 14:52:17 +0800

    header complete

--- elf-parser.c
+++ elf-parser.c
@@ -7,6 +7,7 @@
 
 void read_header(FILE* fp) {
   int i = 0;
+  printf("\n");
 
   /******ident******/
   fread(header.e_ident,16,1,fp);
@@ -16,20 +17,35 @@ void read_header(FILE* fp) {
   printf("    版本: %s\n\n", EV_CURRENT);
 
   /******type/machine/version******/
-  unsigned short tmp[4];
-  fread(tmp,2,3,fp);
-
-  header.e_type = tmp[0];
+  unsigned short t1[3];
+  fread(t1,2,2,fp);
+  header.e_type = t1[0];
   if (header.e_type == 0xff00) printf("%s\n", str_e_type[5]);
   else if (header.e_type == 0xffff) printf("%s\n", str_e_type[6]);
-  else printf("文件类型:  %s\n", str_e_type[header.e_type]);
+  else printf("  文件类型:  %s\n", str_e_type[header.e_type]);
+  header.e_machine = t1[1]; printf("  处理器:  %s\n", str_e_machine[header.e_machine > 10 ? 10 : header.e_machine]);
 
-  header.e_machine = tmp[1];
-  printf("处理器:  %s\n", str_e_machine[header.e_machine > 10 ? 10 : header.e_machine]);
+  /******version/entry/phoff/shoff******/
+  int t2[6];
+  fread(t2,4,5,fp);
+  header.e_version = t2[0]; printf("  版本:  %s\n", str_e_version[header.e_version]);
+  header.e_entry = t2[1];   printf(" >入口偏移: 0x%x\n", header.e_entry);
+  header.e_phoff = t2[2];   printf(" >程序头偏移:  0x%x\n", header.e_phoff);
+  header.e_shoff = t2[3];   printf(" >节头偏移: 0x%x\n", header.e_shoff);
+  header.e_flags = t2[4];   printf("  标志: 0x%x\n\n", header.e_flags);
 
-  header.e_version = tmp[2];
-  printf("版本:  %s\n", str_e_version[header.e_version]);
+  /******ehsize/entry/phoff/shoff******/
+  unsigned short t3[7];
+  fread(t3,2,6,fp);
+  header.e_ehsize = t3[0];    printf("  文件头大小:  0x%x\n", header.e_ehsize);
+  header.e_phentsize = t3[1]; printf("  程序头单个表项大小:  0x%x\n", header.e_phentsize);
+  header.e_phnum = t3[2];     printf("  程序头表项个数:  0x%x\n", header.e_phnum);
+  header.e_shentsize = t3[3]; printf("  节头单个表项大小: 0x%x\n", header.e_shentsize);
+  header.e_shnum = t3[4];     printf("  节头表项个数: 0x%x\n", header.e_shnum);
+  header.e_shstrndx = t3[5];  printf("  节头表项名字索引: 0x%x\n", header.e_shstrndx);
 
+
+  printf("\n");
 }
 
 
--- elf.h
+++ elf.h
@@ -4,11 +4,11 @@ typedef struct ELFHEADER {
     unsigned char e_ident[EI_NIDENT];
     unsigned short e_type;
     unsigned short e_machine;
-    unsigned short e_version;
+    int e_version;
     int e_entry;
     int e_phoff;
     int e_shoff;
-    unsigned short e_flags;
+    int e_flags;
     unsigned short e_ehsize;
     unsigned short e_phentsize;
     unsigned short e_phnum;
